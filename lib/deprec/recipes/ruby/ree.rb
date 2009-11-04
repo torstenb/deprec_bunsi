161b18faf886498da82dc377209c54b0147568d8
Torsten Budesheim <torstenb@gmail.com>
2009-11-03 19:55:00 -0800

    use REE1.8.7, add install option, correct path issues

--- lib/deprec/recipes/ruby/ree.rb
+++ lib/deprec/recipes/ruby/ree.rb
@@ -2,23 +2,29 @@
 Capistrano::Configuration.instance(:must_exist).load do 
   namespace :deprec do     
     namespace :ree do
-      set :ree_version, 'ruby-enterprise-1.8.6-20090610'
+      set :ree_version, 'ruby-enterprise-1.8.7-20090928'
       set :ree_install_dir, "/opt/#{ree_version}"
       set :ree_short_path, '/opt/ruby-enterprise'
       
       SRC_PACKAGES[:ree] = {
-        :md5sum => "0bf66ee626918464a6eccdd83c99d63a #{ree_version}.tar.gz",
-        :url => "http://rubyforge.org/frs/download.php/58677/#{ree_version}.tar.gz",
+#        :md5sum => "0bf66ee626918464a6eccdd83c99d63a #{ree_version}.tar.gz",
+        :url => "http://rubyforge.org/frs/download.php/64475/ruby-enterprise-1.8.7-20090928.tar.gz",
         :configure => '',
         :make => '',
         :install => "./installer --auto /opt/#{ree_version}"
       }
  
+      task :install do
+        install_from_src
+      end
+      
       task :install_from_src do
         install_deps
         deprec2.download_src(SRC_PACKAGES[:ree], src_dir)
         deprec2.install_from_src(SRC_PACKAGES[:ree], src_dir)
         symlink_ree
+        set (:ruby_bin_dir) { "#{ree_short_path}"}
+        set (:ruby_lib_dir) { "#{ree_short_path}/lib/ruby" }
       end
       
       task :install_deps do
