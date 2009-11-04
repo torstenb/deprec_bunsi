71a02b57cb1beab34624f7d9a89d689aac63d5e6
Torsten Budesheim <torstenb@gmail.com>
2009-11-03 19:59:31 -0800

    add upgrade option

--- lib/deprec/recipes/phpmyadmin.rb
+++ lib/deprec/recipes/phpmyadmin.rb
@@ -3,18 +3,20 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deprec do
     namespace :phpmyadmin do
 
-        set :phpmyadmin_base_dir, '/usr/share/phpmyadmin'  # can't be changed
-
+        set :phpmyadmin_std_dir, '/usr/share/phpmyadmin'  # can't be changed
+        set :phpmyadmin_base_dir, '/usr/share/phpmyadmin.current'  # can't be changed
+        set :phpmyadmin_src, 'phpMyAdmin-3.2.2.1-all-languages'
         set :phpmyadmin_port, 8080
         set :phpmyadmin_log_dir, '/var/log'
         set :phpmyadmin_server, 'localhost'
                 
         SRC_PACKAGES[:phpmyadmin] = {
-          :md5sum => "5db3204d57436a032f899ff9dbce793f  lighttpd-1.4.18.tar.gz", 
-          :url => "http://www.lighttpd.net/download/lighttpd-1.4.18.tar.bz2",
-          :configure => "./configure;",
-          :make => "make;",
-          :install => "cp src/spawn-fcgi /usr/bin/spawn-fcgi;"
+          :md5sum => "2433403efdb5347d741846f0c9456773  phpMyAdmin-3.2.2.1-all-languages.tar.bz2", 
+          :url => "http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/3.2.2.1/phpMyAdmin-3.2.2.1-all-languages.tar.bz2",
+          :configure => "",
+          :make => "",
+          :install => "cp -r #{src_dir}/#{phpmyadmin_src} #{phpmyadmin_base_dir}; ",
+          :post_install => "cp -f #{phpmyadmin_std_dir}/config.inc.php #{phpmyadmin_base_dir}; "
         }
 
         SYSTEM_CONFIG_FILES[:phpmyadmin] = [
@@ -39,6 +41,7 @@ Capistrano::Configuration.instance(:must_exist).load do
         task :install do
           apt.install( {:base => 'phpmyadmin'}, :stable )
           top.deprec.mod_rails.rem_apache unless web_server_type == :apache
+          upgrade
           initial_config
         end
 
@@ -46,6 +49,11 @@ Capistrano::Configuration.instance(:must_exist).load do
           SYSTEM_CONFIG_FILES[:phpmyadmin].each do |file|
             deprec2.render_template(:phpmyadmin, file.merge(:remote => true))
           end
+        end
+
+        task :upgrade do
+          deprec2.download_src(SRC_PACKAGES[:phpmyadmin], src_dir)
+          deprec2.install_from_src(SRC_PACKAGES[:phpmyadmin], src_dir)
         end
 
     end
