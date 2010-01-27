f8b790701543c93d1fb0af89705cc1f603ac633c
Torsten Budesheim <torstenb@gmail.com>
2010-01-26 17:54:04 -0800

    - updated to version 3.2.4 - changed/added logdir option and user/group option - added :activate and :deactivate tasks

--- lib/deprec/recipes/phpmyadmin.rb
+++ lib/deprec/recipes/phpmyadmin.rb
@@ -1,18 +1,23 @@
 # Copyright 2009 by Torsten Budesheim. All rights reserved.
 Capistrano::Configuration.instance(:must_exist).load do
+
+  require File.join(File.dirname(__FILE__), '/app/mod_rails.rb')
+  
   namespace :deprec do
     namespace :phpmyadmin do
 
         set :phpmyadmin_std_dir, '/usr/share/phpmyadmin'  # can't be changed
         set :phpmyadmin_base_dir, '/usr/share/phpmyadmin.current'  # can't be changed
-        set :phpmyadmin_src, 'phpMyAdmin-3.2.2.1-all-languages'
+        set :phpmyadmin_src, 'phpMyAdmin-3.2.4-all-languages'
         set :phpmyadmin_port, 8080
-        set :phpmyadmin_log_dir, '/var/log'
+        set(:phpmyadmin_log_dir) { "#{nginx_install_dir}/logs" }
         set :phpmyadmin_server, 'localhost'
+        set :phpmyadmin_user, exists?(:runner) ? fetch(:runner) : 'www-data'
+        set :phpmyadmin_group, exists?(:group) ? fetch(:group) : 'adm'
                 
         SRC_PACKAGES[:phpmyadmin] = {
-          :md5sum => "2433403efdb5347d741846f0c9456773  phpMyAdmin-3.2.2.1-all-languages.tar.bz2", 
-          :url => "http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/3.2.2.1/phpMyAdmin-3.2.2.1-all-languages.tar.bz2",
+          :md5sum => "b927655abd701d8e35079f9e5ec24ee2  phpMyAdmin-3.2.4-all-languages.tar.bz2", 
+          :url => "http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/3.2.4/phpMyAdmin-3.2.4-all-languages.tar.bz2",
           :configure => "",
           :make => "",
           :install => "cp -r #{src_dir}/#{phpmyadmin_src} #{phpmyadmin_base_dir}; ",
@@ -22,19 +27,19 @@ Capistrano::Configuration.instance(:must_exist).load do
         SYSTEM_CONFIG_FILES[:phpmyadmin] = [
 
           {:template => 'phpmyadmin.erb',
-            :path => nginx_vhost_dir + '/phpmyadmin.conf',
+            :path => nginx_vhost_avail + '/phpmyadmin.conf',
             :mode => 0644,
-            :owner => 'www-data:adm'},
+            :owner => "#{phpmyadmin_user}:#{phpmyadmin_group}"}
 
-          {:template => 'empty.log',
-            :path => phpmyadmin_log_dir + '/access.log',
-            :mode => 0644,
-            :owner => 'www-data:adm'},
+          #{:template => 'empty.log',
+          #  :path => phpmyadmin_log_dir + '/access.log',
+          #  :mode => 0644,
+          #  :owner => "#{phpmyadmin_user}:#{phpmyadmin_group}"},
 
-          {:template => 'empty.log',
-            :path => phpmyadmin_log_dir + '/error.log',
-            :mode => 0644,
-            :owner => 'www-data:adm'}
+          #{:template => 'empty.log',
+          #  :path => phpmyadmin_log_dir + '/error.log',
+          #  :mode => 0644,
+          #  :owner => "#{phpmyadmin_user}:#{phpmyadmin_group}"}
         ]
 
         desc "Install phpmyadmin"
@@ -54,6 +59,24 @@ Capistrano::Configuration.instance(:must_exist).load do
         task :upgrade do
           deprec2.download_src(SRC_PACKAGES[:phpmyadmin], src_dir)
           deprec2.install_from_src(SRC_PACKAGES[:phpmyadmin], src_dir)
+        end
+
+        desc "Deactivate phpmyadmin on Nginx+Passenger"
+        task :deactivate, :roles => :web do
+          if app_server_type == :mod_rails then
+            sudo "test -f #{nginx_vhost_dir}/phpmyadmin.conf && #{sudo} rm #{nginx_vhost_dir}/phpmyadmin.conf || exit 0"
+          else
+            puts 'Sorry, this task doesn\'t work with your configuration'
+          end
+        end
+
+        desc "Activate phpmyadmin on Nginx+Passenger"
+        task :activate, :roles => :web do
+          if app_server_type == :mod_rails then
+            sudo "test -f #{nginx_vhost_dir}/phpmyadmin.conf || #{sudo} ln -sf #{nginx_vhost_avail}/phpmyadmin.conf #{nginx_vhost_dir}/phpmyadmin.conf"
+          else
+            puts 'Sorry, this task doesn\'t work with your configuration'
+          end
         end
 
     end
